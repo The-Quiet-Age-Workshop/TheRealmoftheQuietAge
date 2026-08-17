33c45038cbe4afe0c9104b2c47b3aa779c603253
AvenkyrPakacit <pwthesnell2017@gmail.com>
2026-08-17 14:20:51 -0500

    Update actions.py

--- data/actions.py
+++ data/actions.py
@@ -148,23 +148,24 @@ class MeleeAction(ActionWithDirection):
         if not target:
             raise exceptions.Impossible("Nothing to attack.")
 
-        damage = max(1, (self.entity.fighter.power - target.fighter.defense))
+        if self.engine.roll_dice(1, 20) != 1:
+            damage = max(1, (self.entity.fighter.power - target.fighter.defense))
+            attack_desc = f"{self.entity.name.capitalize()} attacks {target.name}"
 
-        attack_desc = f"{self.entity.name.capitalize()} attacks {target.name}"
-        if self.entity is self.engine.player:
-            attack_color = color.player_atk
-        else:
-            attack_color = color.enemy_atk
+            if self.entity is self.engine.player:
+                attack_color = color.player_atk
+            else:
+                attack_color = color.enemy_atk
 
-        if damage > 0:
-            self.engine.message_log.add_message(
-                f"{attack_desc} for {damage} hit points.", attack_color
-            )
-            target.fighter.hp -= damage
-        else:
-            self.engine.message_log.add_message(
-                f"{attack_desc} but does no damage.", attack_color
-            )
+            if damage > 0:
+                self.engine.message_log.add_message(
+                    f"{attack_desc} for {damage} hit points.", attack_color
+                )
+                target.fighter.hp -= damage
+            else:
+                self.engine.message_log.add_message(
+                    f"{attack_desc} but does no damage.", attack_color
+                )
 
 
 class MovementAction(ActionWithDirection):
